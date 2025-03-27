067b966a884e3f09c9bff21233f9b189e4e2fa11
Gevorji <techno9882@gmail.com>
2025-03-27 11:11:09 +0300

    Add session.py

---
+++ src/currency_exchange/db/session.py
@@ -0,0 +1,17 @@
+from sqlalchemy import URL
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from currency_exchange.config import db_conn_settings, general_settings
+
+engine = create_async_engine(
+    URL.create(
+        drivername=f'{db_conn_settings.DBMS}+{db_conn_settings.DRIVER}',
+        host=db_conn_settings.HOST,
+        port=db_conn_settings.PORT,
+        username=db_conn_settings.USERNAME,
+        password=db_conn_settings.PASSWORD,
+        database=db_conn_settings.DB_NAME,
+    ), echo=general_settings.DEBUG
+)
+
+async_session_factory = async_sessionmaker(engine)
