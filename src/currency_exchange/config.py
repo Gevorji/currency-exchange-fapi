8137ea0a84585d396c47299cfd18cd818d2f5327
Gevorji <techno9882@gmail.com>
2025-04-03 22:01:57 +0300

    Add auth config

--- src/currency_exchange/config.py
+++ src/currency_exchange/config.py
@@ -33,5 +33,32 @@ class DbConnectionSettings(BaseSettings):
     def validate_host(cls, value):
         return value.lower()
 
+class AuthConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', env_prefix='AUTH_', extra='ignore')
+
+    class JWTAlgorithms(Enum):
+        HS256 = 'HS256'
+        RS256 = 'RS256'
+        ES256 = 'ES256'
+
+    JWT_ISSUER: str = 'gevorji.currency-exchange.auth'
+    JWT_SIGN_ALGORITHM: JWTAlgorithms = JWTAlgorithms.RS256
+    JWT_PUB_KEY_PATH: Path
+    JWT_PRIV_KEY_PATH: Path
+    JWT_ACCESS_DURATION: timedelta = timedelta(seconds=1800)
+    JWT_REFRESH_DURATION: timedelta = timedelta(seconds=1209600)
+    JWT_NBF_DURATION: Optional[timedelta] = None
+    JWT_JTIS: bool = True
+    PASSWORD_VALIDATORS: list[str] = [
+        'currency_exchange.auth.services.passwordvalidation.min_length_validator',
+        'currency_exchange.auth.services.passwordvalidation.no_whitespace_chars_validator'
+    ]
+    PASSWORD_HASHERS: list[str] = [
+        'currency_exchange.auth.services.passwordhashing.BCryptHasher'
+    ]
+    MIN_PASSWORD_LENGTH: int = 8
+    USERNAME_MIN_LENGTH: int = 5
+
 db_conn_settings = DbConnectionSettings()
 general_settings = GeneralSettings()
+auth_settings = AuthConfig()
