3912d8ff00b304de4cf9d7d41dbae3521b270246
Gevorji <techno9882@gmail.com>
2025-04-21 20:51:04 +0300

    Fix import of JWTAlgorithms.

--- src/currency_exchange/auth/services/jwtservice/__init__.py
+++ src/currency_exchange/auth/services/jwtservice/__init__.py
@@ -1,7 +1,5 @@
-from jwtvalidator import JWTValidator
-from jwtissuer import JWTIssuer
-from jwtmodel import JWTModel
-
 from currency_exchange.config import AuthConfig as _AuthConfig
-
 JWTAlgorithms = _AuthConfig.JWTAlgorithms
+from .jwtvalidator import JWTValidator
+from .jwtissuer import JWTIssuer
+from .jwtmodel import JWTModel
