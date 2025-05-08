8f9104038aa79b3ff9068c8d221f7a36ced155d9
Gevorji <techno9882@gmail.com>
2025-05-08 14:05:50 +0300

    Move db connection url to top level variable.

--- src/currency_exchange/db/session.py
+++ src/currency_exchange/db/session.py
@@ -3,15 +3,15 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from currency_exchange.config import db_conn_settings, general_settings
 
-engine = create_async_engine(
-    URL.create(
+url = URL.create(
         drivername=f'{db_conn_settings.DBMS}+{db_conn_settings.DRIVER}',
         host=db_conn_settings.HOST,
         port=db_conn_settings.PORT,
         username=db_conn_settings.USERNAME,
         password=db_conn_settings.PASSWORD,
         database=db_conn_settings.DB_NAME,
-    ), echo=general_settings.DEBUG
-)
+    )
+
+engine = create_async_engine(url, echo=general_settings.DEBUG)
 
 async_session_factory = async_sessionmaker(engine, expire_on_commit=False)
