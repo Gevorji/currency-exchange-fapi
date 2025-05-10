1502639d811db6b597e48665278f0b505479863f
Gevorji <techno9882@gmail.com>
2025-05-10 14:20:24 +0300

    Create main.py.

---
+++ src/currency_exchange/auth/main.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from .routes import clients_router, token_router
+from .admin import admin_router as  auth_admin_router
+
+
+auth_router = APIRouter()
+auth_router.include_router(clients_router, prefix='/clients', tags=['auth'])
+auth_router.include_router(token_router, prefix='/tokens', tags=['auth'])
