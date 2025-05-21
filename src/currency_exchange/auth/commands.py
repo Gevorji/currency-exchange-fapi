73794e7ffe4d1209e6d559e2b18b66bcba57477f
Gevorji <techno9882@gmail.com>
2025-05-21 16:00:52 +0300

    Add generatecryptkeys command.

--- src/currency_exchange/auth/commands.py
+++ src/currency_exchange/auth/commands.py
@@ -1,8 +1,10 @@
 import asyncio
 import datetime
 from getpass import getpass
+import argparse
 
 from sqlalchemy import delete
+from joserfc.jwk import RSAKey
 
 from currency_exchange.db.session import async_session_factory
 from .dbmodels import User, UserCategory, TokenState
@@ -50,3 +52,17 @@ async def _remove_expired_tokens():
 				.returning(TokenState.id)
 			)
 		print(f"Removed {len(res.fetchall())} expired tokens")
+
+
+def generate_crypto_keys():
+	parser = argparse.ArgumentParser(prog='generatecryptokeys', usage='%(prog)s [options]')
+	parser.add_argument('--pub', type=str, help='Public key path', required=True)
+	parser.add_argument('--priv', type=str, help='Private key path', required=True)
+	args = parser.parse_args()
+
+	key = RSAKey.generate_key()
+	with (open(args.priv, "wb") as priv_key_file,
+		  open(args.pub, "wb") as pub_key_file):
+		priv_key_file.write(key.as_pem())
+		pub_key_file.write(key.as_pem(private=False))
+
